b3e396dd1960e1618003b9c28e6c786b3e51c360
Alexey <kuzhakov.fx@gmail.com>
2022-11-21 18:39:27 +0300

    Добавил операции python

--- lecture1/helloPython.py
+++ lecture1/helloPython.py
@@ -14,3 +14,12 @@ if a in list:
     print(f'Число {a} есть в списке')
 else:
     print(f'Числа {a} нет в списке')
+
+print('Введите ваше имя')
+name = input()
+if name == 'Алексей':
+    print('Добрый день', name)
+elif name == 'Леша':
+    print('Привет', name)
+else:
+    print(name, ', вас не ждали')
